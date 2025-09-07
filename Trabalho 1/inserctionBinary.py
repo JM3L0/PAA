70c2e728522778851f99bea1b4393f2771a061f7
Seu Nome <seuemail@exemplo.com>
2025-09-07 15:58:41 -0300

    bom, codigos ok

--- Trabalho 1/inserctionBinary.py
+++ Trabalho 1/inserctionBinary.py
@@ -1,12 +1,21 @@
 import time, os
 
-def insertion_sort(arr, tam):
+def binary_search(arr, chave, start, end):
+    while start < end:
+        mid = (start + end) // 2
+        if arr[mid] < chave:
+            start = mid + 1
+        else:
+            end = mid
+    return start
+
+def binary_insertion_sort(arr, tam):
     for i in range(1, tam):
-        chave, j = arr[i], i - 1
-        while j >= 0 and arr[j] > chave:
-            arr[j + 1] = arr[j]
-            j -= 1
-        arr[j + 1] = chave
+        chave = arr[i]
+        pos = binary_search(arr, chave, 0, i)
+        for j in range(i, pos, -1):
+            arr[j] = arr[j-1]
+        arr[pos] = chave
     return arr
 
 def ler_numeros(caminho):
@@ -26,17 +35,17 @@ def medir_tempo(arquivos, pasta_resultados="Trabalho 1/resultados"):
         tam = len(nums)
         
         inicio = time.time()
-        insertion_sort(nums, tam)
+        binary_insertion_sort(nums, tam)
         tempo = time.time() - inicio
         
         resultados.append((arq, tam, tempo))
         print(f"{arq}: {tempo:.6f} s")
 
-    with open(os.path.join(pasta_resultados, "tempos_insertion_sort.txt"), "w") as f:
+    with open(os.path.join(pasta_resultados, "tempos_binary_insertion_sort.txt"), "w") as f:
         f.write("Arquivo\tQuantidade\tTempo(s)\n")
         for arq, q, t in resultados:
             f.write(f"{arq}\t{q}\t{t:.6f}\n")
-    print(f"\nResultados salvos em '{pasta_resultados}/tempos_insertion_sort.txt'")
+    print(f"\nResultados salvos em '{pasta_resultados}/tempos_binary_insertion_sort.txt'")
 
 # --- USO ---
 arquivos = [f"Trabalho 1/entradas/{tipo}_{n}.txt" 
